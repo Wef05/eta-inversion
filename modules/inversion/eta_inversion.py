131b12ecdef3a4b4e76e588d49aaa70a82e6f200
Wef05 <limingzhe1110@gmail.com>
2025-08-01 14:14:20 +0800

    fix:设备不匹配

--- modules/inversion/eta_inversion.py
+++ modules/inversion/eta_inversion.py
@@ -146,8 +146,8 @@ class EtaInversion(DiffusionInversion):
 
         self.anti_gradient = AntiGradientPipeline(self.model,self.scheduler_bwd)
         #实例化controlnet
-        controlnet_model = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
-        self.controlnet = ControlNetPaperer(controlnet_model,self.scheduler_bwd.timesteps).to(self.model.device)
+        controlnet_model = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float32).to(self.model.device)
+        self.controlnet = ControlNetPaperer(controlnet_model,self.scheduler_bwd.timesteps)
         if eta_start is not None:
             # for gradio
             assert eta_end is not None
